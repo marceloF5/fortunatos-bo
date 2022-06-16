129a88c7b02023e6568a2d9e38f8564fd732667b
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2022-06-17 00:32:37 +0100

    initial commit

---
+++ config/database.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+    connection: {
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'fortunatos-db'),
+        user: env('DATABASE_USERNAME', 'marcelo.fortunato'),
+        password: env('DATABASE_PASSWORD', 'F31j04d4'),
+        ssl: env.bool('DATABASE_SSL', false),
+      }
+    },
+  })
